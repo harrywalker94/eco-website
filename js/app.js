ce4ad39f40fdc1b679b2a03f0238f2317d8176e2
harry walker <harrywalker312@gmail.com>
2020-02-23 14:49:54 +0000

    feat: googlemapsAPI, aboutus.html

--- js/app.js
+++ js/app.js
@@ -9,43 +9,6 @@ $(document).ready(function () {
     $(".navbar-toggler").toggleClass("change");
   });
 
-  /* Icon hover color change */
-  $(".icon-top a:nth-child(4)").hover(function () {
-    $(".icon-hover").css("color", "#f1fffa");
-    $(".contact-us1").css("color", "#214c32");
-  }, function () {
-    $(".icon-hover").css("color", "#214c32");
-    $(".contact-us1").css("color", "#f1fffa");
-  });
-
-  $(".icon-top a:nth-child(5)").hover(function () {
-    $(".icon-hover2").css("color", "#f1fffa");
-    $(".contact-us2").css("color", "#214c32");
-  }, function () {
-    $(".icon-hover2").css("color", "#214c32");
-    $(".contact-us2").css("color", "#f1fffa");
-
-  });
-
-
-
-  $(".icon-top a:nth-child(4)").hover(function () {
-    $(".icon-hover").css("color", "#f1fffa");
-  }, function () {
-    $(".icon-hover").css("color", "#214c32");
-  });
-
-  $(".icon-top a:nth-child(5)").hover(function () {
-    $(".icon-hover2").css("color", "#f1fffa");
-  }, function () {
-    $(".icon-hover2").css("color", "#214c32");
-  });
-
-
-
-
-
-
   /* Navbar Fade In */
   // Hide Navbar initially
   $(function () {
@@ -61,8 +24,16 @@ $(document).ready(function () {
     });
   });
 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 60) {
+      $('#topBtn').fadeIn();
+    } else {
+      $('#topBtn').fadeOut();
+    }
+  });
+
   /* Smooth Scroll */
-  $(".nav-item a").click(function (link) {
+  $(".nav-item a, .top-icons a, #topBtn").click(function (link) {
     link.preventDefault(); // preventDefault method stops the default action of an element happening
 
     let target = $(this).attr("href"); // Find the attribute of the href of the anchor link
@@ -80,36 +51,75 @@ $(document).ready(function () {
 
 });
 
-
-
-
+/* GoogleMaps API */
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    /* Map Options */
+    zoom: 13,
     center: {
-      lat: 46.619261,
-      lng: -33.134766
+      lat: 51.739300,
+      lng: 0.048120
     }
   });
 
-  var labels = ["ECO", "ECO", "ECO", "ECO"];
-
-  var locations = [
-    { lat: 51.727798, lng: 0.020863 },
-    { lat: 51.761559, lng: 0.004545 },
-    { lat: 51.800944, lng: 0.146734 },
-    { lat: 51.766047, lng: 0.105656 }
-  ];
-
-  var markers = locations.map(function (location, i) {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length]
-    });
+  var marker = new google.maps.Marker({
+    position: { lat: 51.739300, lng: 0.048120 },
+    map: map,
+    icon: 'http://maps.google.com/mapfiles/kml/paddle/grn-blank.png',
+    draggable: true,
+    animation: google.maps.Animation.DROP,
   });
-  var markerCluster = new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+
+  marker.addListener('click', toggleBounce);
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<img src="assets/images/newlogo3.png"/> '
+  });
+
+  marker.addListener('click', function () {
+    infoWindow.open(map, marker);
+  });
+
+  var circle = new google.maps.Circle({
+    map: map,
+    center: {
+      lat: 51.727798,
+      lng: 0.020863
+    },
+    radius: 100000
+
+  });
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+/* EmailJS
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // generate the contact number value
+    this.contact_number.value = Math.random() * 100000 | 0;
+    emailjs.sendForm('contact_service', 'contact_template', this);
   });
 }
+*/
+
+
+
 
