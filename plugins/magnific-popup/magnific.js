c09d98452a2e0989776cb80264cdb783d1fe1c1e
harry walker <harrywalker312@gmail.com>
2020-04-15 14:24:00 +0100

    js: fixing magnific.js; html: magnific btn; css: style magnific btn

--- plugins/magnific-popup/magnific.js
+++ plugins/magnific-popup/magnific.js
@@ -1,141 +1,18 @@
 $(document).ready(function () {
 
   $('.pop-up').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
+    delegate: 'a',
     type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
     gallery: {
-      enabled: true
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
     }
-    // other options
   });
 
-
-  /* Magnific Image gallery */
-  // Solar PV
-  $('#open-popup1').magnificPopup.open({
-    items: [
-      {
-        src: '../assets/images/solar-pv/solarpv-diagram.png',
-        title: 'Solar PV Diagram'
-      },
-      {
-        src: '../assets/images/solar-pv/solar6.jpg',
-        title: 'Solar PV'
-      },
-      {
-        src: '../assets/images/solar-pv/solar1.jpg',
-        title: 'Solar PV'
-      }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image', // this is a default type
-
-  });
-  // Air-Source Heat Pump
-  $('#open-popup2').magnificPopup({
-    items: [
-      {
-        src: '../assets/images/air-source/air-source-diagram.jpg',
-        title: 'Air Source Heat Pump Diagram'
-      },
-      {
-        src: '../assets/images/air-source/air-source4.jpg',
-        title: 'Air Source Heat Pump'
-      },
-      {
-        src: '../assets/images/air-source/air-source6.jpg',
-        title: 'Air Source Heat Pump'
-      }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-
-  $('#open-popup3').magnificPopup({
-    items: [
-      {
-        src: '../assets/images/battery-storage/battery-storage-diagram.png',
-        title: 'Battery Storage System Diagram'
-      },
-      {
-        src: '../assets/images/battery-storage/battery-storage1.jpg',
-        title: 'Battery Storage System'
-      },
-      {
-        src: '../assets/images/battery-storage/battery-storage3.png',
-        title: 'Battery Storage System'
-      }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-
-  $('#open-popup4').magnificPopup({
-    items: [
-      {
-        src: '../assets/images/inverter-systems/inverter-diagram.png',
-        title: 'Inverter System Diagram'
-      },
-      {
-        src: '../assets/images/inverter-systems/solaredge2.jpg',
-        title: 'Inverter System'
-      },
-      {
-        src: '../assets/images/inverter-systems/solaredge4.jpg',
-        title: 'Inverter System'
-      }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-
-  $('#open-popup5').magnificPopup({
-    items: [
-      {
-        src: '../assets/images/air-to-air/air-conditioner1.jpg',
-        title: 'Air to Air Unit'
-      },
-      {
-        src: '../assets/images/air-to-air/air-conditioner2.jpg',
-        title: 'Air to Air Unit'
-      },
-      {
-        src: '../assets/images/air-to-air/air-conditioner3.jpg',
-        title: 'Air to Air Unit'
-      }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-
-  $('#open-popup6').magnificPopup({
-    items: [
-      {
-        src: '../assets/images/gas-boilers/boiler1.jpg',
-        title: 'Gas Boiler Diagram'
-      },
-      {
-        src: '../assets/images/gas-boilers/boiler4.jpg',
-        title: 'Gas Boiler'
-      },
-      {
-        src: '../assets/images/gas-boilers/boiler3.jpg',
-        title: 'Gas Boiler'
-      }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
 });
